c9014e6fd55f935012e3cc98224ad3b6a4da686f
aripoi <106456245+aripoi@users.noreply.github.com>
2022-10-31 16:19:31 +0900

    Delete 10-1-3 directory

--- 10-1-3/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PageColor from './chapter12/PageColor';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <PageColor />
-);
